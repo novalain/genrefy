035850348ac8eed80ba1c9a4975cd9797cec1d35
Oskar Ankarberg <oankarberg@gmail.com>
2016-09-29 16:33:51 +0200

    read in the audio files as frames. skeleton for extending or changing the representation

---
+++ server/read_audio.py
@@ -0,0 +1,29 @@
+
+
+import numpy as np 
+
+from scipy.io.wavfile import write
+import sounddevice as sd
+import sunau 
+
+#The music files contains of 22050Hz sample and 30 seconds. Which is: 30 * 22050 = 661500 frames. 
+GENRES = ["jazz", "rock", "disco", "hiphop"]
+FILE_PATH = "../dataset/genres/"
+file_name_prefix = '00000'
+NR_FRAMES = 661800 #Above number of  framesis sufficient 
+audio_data_dic = {}
+def readAUFiles():
+  for k in range(len(GENRES)):
+    audio_data = []
+    for i in range(100): 
+      #String operation to get the filename
+      file_name = file_name_prefix[:-len(str(i))] + str(i)
+      f = sunau.Au_read(FILE_PATH+GENRES[k]+ '/' + GENRES[k] +'.'+ file_name +'.au')
+      audio_data = np.fromstring(f.readframes(NR_FRAMES), dtype=np.int16)
+
+    audio_data_dic[GENRES[k]] = audio_data
+
+
+readAUFiles()
+print "Dictionary initialized: " 
+print audio_data_dic.keys()
