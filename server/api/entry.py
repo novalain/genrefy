1ebd0380937ec57aa59b48dcf81926523dbddad4
novalain <michael.f.noven@gmail.com>
2016-10-05 13:05:23 +0200

    Make python API take JSON and binary data

--- server/api/entry.py
+++ server/api/entry.py
@@ -1,9 +1,23 @@
 from flask import Flask, url_for
+from flask import request
 app = Flask(__name__)
 
-@app.route('/')
-def api_root():
-    return 'Hello world'
+from flask import json
+
+@app.route('/messages', methods = ['POST'])
+def api_message():
+  if request.headers['Content-Type'] == 'text/plain':
+    return "Text Message: " + request.data
+  elif request.headers['Content-Type'] == 'application/json':
+    return "JSON Message: " + json.dumps(request.json)
+  elif request.headers['Content-Type'] == 'application/octet-stream':
+    # Open file and write binary (blob) data
+    f = open('./binary', 'wb')
+    f.write(request.data)
+    f.close()
+    return "Binary message written!"
+  else:
+    return "415 unsupported media type"
 
 if __name__ == '__main__':
-    app.run()
+  app.run()
