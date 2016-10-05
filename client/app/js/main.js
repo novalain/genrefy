e422591be1541aaa17780cd0a9267a5a6217fab4
novalain <michael.f.noven@gmail.com>
2016-10-05 11:03:05 +0200

    Create simple audio recorder class

--- client/app/js/main.js
+++ client/app/js/main.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+window.onload = () => {
+  const ar = new AudioRecorderManager();
+  ar.ready().then(() => {
+    ar.start();
+    console.log('start');
+    setTimeout(() => {
+      ar.stop();
+      console.log("blob", ar.blob);
+    }, 1000);
+  });
+}
